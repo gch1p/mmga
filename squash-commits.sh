33bb9f3275f0e2f9865a1a5c0d7692d0df580419
Evgeny Zinoviev <me@ch1p.io>
2019-06-20 15:04:29 +0300

    remove some script not related to this repo

--- squash-commits.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-git checkout --orphan new-master
-git add .
-git commit -m "$1"
-git branch -m master old-master
-git branch -m new-master master
-git push --force --set-upstream origin master
-git branch -D old-master
-git push
